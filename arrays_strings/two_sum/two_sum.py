737d0269a5e63c12ab638c976c794ec9a6a9774a
James H. <lsaucyfx@gmail.com>
2024-02-05 13:02:38 -0500

    solved two sum and started hash map

---
+++ arrays_strings/two_sum/two_sum.py
@@ -0,0 +1,19 @@
+def two_sum(self, nums, val):
+
+        self.init = __init__
+        nums = []
+        val = val
+        max = len(nums)
+        #sort the array
+        nums = sorted(nums)
+
+        # BRUTE FORCE APPROACH
+
+        # iterate through the array 
+        for i in range(len(nums)):
+            for j in range(i+1, len(input())):
+        # check if indices add to val     
+                if i + j == val:
+                    return true
+        #t-complexity : O(n^2)
+       
